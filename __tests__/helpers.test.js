a546cc78ca3bf00527dda42975f8f55c821f2e23
Menji <jennifer.menjivar18@gmail.com>
2021-10-04 10:26:36 -0400

    added edit/delete posts to dashboard

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -15,3 +15,11 @@ test("format_url() returns a simplified url string", () => {
   expect(url2).toBe("coolstuff.com");
   expect(url3).toBe("google.com");
 });
+
+test("format_plural() returns a pluralized word", () => {
+  const word1 = format_plural("tiger", 1);
+  const word2 = format_plural("lion", 2);
+
+  expect(word1).toBe("tiger");
+  expect(word2).toBe("lions");
+});
