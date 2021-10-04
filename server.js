9868d046f368d025f965e139422eb52c9ca178f5
Menji <jennifer.menjivar18@gmail.com>
2021-10-03 21:44:13 -0400

    added sessions and front end logic for users

--- server.js
+++ server.js
@@ -3,9 +3,24 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
 
+require("dotenv").config();
+
 // set up Handlebars
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
+const session = require("express-session");
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -17,6 +32,9 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+// middleware for sessions
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
